ce3051cbd71aa3365e785df419af461cb45ce794
AnHaechan <mike000529@gmail.com>
2023-08-19 16:53:31 +0000

    grading script for 12 fixed

--- scripts/grade-12.sh
+++ scripts/grade-12.sh
@@ -24,7 +24,7 @@ run_linters || exit 1
 for RUNNER in "${RUNNERS[@]}"; do
     echo "Running with $RUNNER..."
 
-    TESTS=("--lib assignment11")
+    TESTS=("--lib assignment12")
     if [ $(run_tests) -ne 0 ]; then
         exit 1
     fi
