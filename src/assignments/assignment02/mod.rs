d28bca2b181bc72cd187879707f68fe0399a6e90
AnHaechan <mike000529@gmail.com>
2023-08-21 07:13:27 +0000

    assignment 1~5: fixes - assignment05/pascal.mlw: lowered the difficulty (one more invariant given) - assignment02, 03: minor fixes & divide into sub-problems

---
+++ src/assignments/assignment02/mod.rs
@@ -0,0 +1,13 @@
+//! Assignment 2: Mastering common programming concepts (1/2).
+//!
+//! The primary goal of this assignment is to re-learn the common programming concepts in Rust, especially those in the Rust Book chapters 3 and 5.
+//! Please make sure you're comfortable with the concepts to proceed on to the next assignments.
+//!
+//! You should fill out the `todo!()` placeholders in such a way that `/scripts/grade-02.sh` works fine.
+//! See `*_grade.rs` and `/scripts/grade-02.sh` for the test script.
+
+pub mod small_problems;
+mod small_problems_grade;
+
+pub mod vec_and_mat;
+mod vec_and_mat_grade;
---
+++ src/assignments/assignment03/custom_operators.rs
@@ -0,0 +1,79 @@
+//! You will implement a number of custom operators.
+
+/// Custom option type.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MyOption<T> {
+    /// Some value of type `T`.
+    MySome(T),
+    /// No value.
+    MyNone,
+}
+
+/// Maps an `MyOption<T>` to `MyOption<U>` by applying a function to a contained value.
+///
+/// # Examples
+///
+/// Converts an `MyOption<String>` into an `MyOption<usize>`, consuming the original:
+///
+/// ```
+/// use cs220::assignments::assignment03::{my_map, MyOption};
+///
+/// fn len(s: String) -> usize {
+///     s.len()
+/// }
+///
+/// assert_eq!(my_map(MyOption::MySome(String::from("Hello, World!")), len), MyOption::MySome(13));
+/// assert_eq!(my_map(MyOption::MyNone, len), MyOption::MyNone);
+/// ```
+pub fn my_map<T, U, F: FnOnce(T) -> U>(v: MyOption<T>, f: F) -> MyOption<U> {
+    todo!()
+}
+
+/// Returns `MyNone` if the option is `MyNone`, otherwise calls `f` with the wrapped value and returns the result.
+///
+/// Some languages call this operation flatmap.
+///
+/// # Examples
+///
+/// ```
+/// use cs220::assignments::assignment03::{MyOption, my_and_then};
+///
+/// fn pos_then_to_string(x: isize) -> MyOption<String> {
+///     if x > 0 {
+///         MyOption::MySome(x.to_string())
+///     } else {
+///         MyOption::MyNone
+///     }
+/// }
+///
+/// assert_eq!(my_and_then(MyOption::MySome(2), pos_then_to_string), MyOption::MySome(2.to_string()));
+/// assert_eq!(my_and_then(MyOption::MySome(-3), pos_then_to_string), MyOption::MyNone);
+/// assert_eq!(my_and_then(MyOption::MyNone, pos_then_to_string), MyOption::MyNone);
+/// ```
+pub fn my_and_then<T, U, F: FnOnce(T) -> MyOption<U>>(v: MyOption<T>, f: F) -> MyOption<U> {
+    todo!()
+}
+
+/// Custom operator: `option_op_or(v1, v2, f)`
+/// If neither `v1` nor `v2` is `Some`, returns `None`.
+/// If exactly one is `Some`, returns the same `Some` value.
+/// If both are `Some`, apply the values inside `Some` to `f` and wrap the resulting value inside `Some`.
+///
+/// # Examples
+///
+/// ```
+/// fn product(a: i32, b: i32) -> i32 {
+///     a * b
+/// }
+///
+/// assert_eq!(option_op_or(None, None, product), None);
+/// assert_eq!(option_op_or(Some(3), None, product), Some(3));
+/// assert_eq!(option_op_or(Some(3), Some(5), product), Some(15));
+/// ```
+pub fn my_option_op_or<T, F: FnOnce(T, T) -> T>(
+    v1: MyOption<T>,
+    v2: MyOption<T>,
+    f: F,
+) -> MyOption<T> {
+    todo!()
+}
