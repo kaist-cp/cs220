1e079091754752141146b2ea1ffb5242ff69e1db
AnHaechan <mike000529@gmail.com>
2023-08-29 02:43:16 +0000

    submission script for why3 assignment

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -9,6 +9,10 @@ mkdir -p $BASEDIR/target
 for i in {01..13} ; do
   if [ $i -eq 5 ]
   then
+    if [ -f $BASEDIR/target/assignment05.zip ]; then
+      rm $BASEDIR/target/assignment05.zip
+    fi
+    zip -rj $BASEDIR/target/assignment05.zip $BASEDIR/assets/why3/assignment05
     continue
   fi
   if [ -f $BASEDIR/target/assignment$i.zip ]; then
